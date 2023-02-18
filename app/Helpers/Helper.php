b1793abe30e8af398bc6209465d8711e7d18c8da
RahulR12345 <r.ranjan099@gmail.com>
2023-02-18 14:32:13 +0530

    create notification controller,migration and model

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -16,32 +16,29 @@ class Helper{
 	    $token = substr(str_shuffle(str_repeat($pool, 5)), 0, $length);
 	    return $token;
 	}
+
 	public static function generateOTP($length){
 		$string		=	'0123456789';
 		$strShuffled=	str_shuffle($string);
 		$otp		=	substr($strShuffled, 1, $length);
 		return $otp;
 	}
-	public static function createUserReferal($length){
+
+	public static function createUserReferal($length)
+	{
 		$string		=	'0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ9630125478abcdefghijklmnopqrstuvwxyz9876543210';
 		$strShuffled=	str_shuffle($string);
 		$referCode	=	substr($strShuffled, 1, $length);
 		return $referCode;
 	}
-	public static function get_tot_users(){
-		return User::where(["role_id"=>4])->count();
-	}
-	public static function get_tot_sub_admins(){
-		return User::where(["role_id"=>2])->count();
-	}
-	public static function get_tot_category(){
-		return Category::count();
-	}
-	public static function pr($arr = []){
+
+	public static function pr($arr = [])
+	{
 		echo "<pre>"; print_r($arr); echo "</pre>";
 	}
-	public static function sendOTP($mob_no,$otp){
 
+	public static function sendOTP($mob_no,$otp)
+	{
 		$curl = curl_init();
 
 		curl_setopt_array($curl, [
